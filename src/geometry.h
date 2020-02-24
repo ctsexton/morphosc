cdbc791124aed99a5188c27f7c93561d99bf77b5
Cam Sexton <cameron.t.sexton@gmail.com>
2020-02-23 17:17:02 -0800

    add geometry file

---
+++ src/geometry.h
@@ -0,0 +1,6 @@
+#ifndef GEOMETRY_H_
+#define GEOMETRY_H_
+
+float add(float a, float b);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <MorphOSCConfig.h>
+#include <geometry.h>
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     printf("Version %d.%d\n", MorphOSC_VERSION_MAJOR, MorphOSC_VERSION_MINOR);
     printf("Usage: %s --address 127.0.0.1 --port 5050\n", argv[0]);
+    printf("1 + 1 = %f", add(1.0, 1.0));
   }
   return 0;
 }
